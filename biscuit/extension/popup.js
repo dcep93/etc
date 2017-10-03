7597c2964773872b95037c5b509df8f5c221cd84
Daniel Cepeda <dcep93@gmail.com>
2017-10-03 12:15:41 -0700

    more link types

--- biscuit/extension/popup.js
+++ biscuit/extension/popup.js
@@ -2,16 +2,20 @@
 	var div = document.getElementById('main');
 	var link = document.getElementById('link');
 
-	link.onclick=function() { this.setSelectionRange(0, this.value.length); };
+	link.onclick = function() {
+		this.setSelectionRange(0, this.value.length);
+	};
 	var tabId;
 
-	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+	chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
 		tabId = tabs[0].id;
 		scrape();
 	});
 
 	function scrape() {
-		chrome.runtime.sendMessage({method: "init", tabId: tabId}, function(initResponse) {
+		chrome.runtime.sendMessage({ method: 'init', tabId: tabId }, function(
+			initResponse
+		) {
 			if (initResponse.success) {
 				getContentAndScrape();
 			}
@@ -19,7 +23,9 @@
 	}
 
 	function getContentAndScrape() {
-		chrome.tabs.sendMessage(tabId, {method: "getContent"}, null, function(contentResponse) {
+		chrome.tabs.sendMessage(tabId, { method: 'getContent' }, null, function(
+			contentResponse
+		) {
 			if (contentResponse) {
 				scrapeFromContent(contentResponse);
 			} else {
@@ -35,18 +41,23 @@
 			var innerHTML = contentResponse.innerHTML;
 
 			var match;
-			var videoRegex = new RegExp(/youtube\.com\/(watch\?v=|embed\/)([A-Za-z0-9\-_]+)/g);
-			while((match = videoRegex.exec(innerHTML)) !== null) {
-				var videoId = match[2];
+			var videoRegex = new RegExp(
+				/(youtube\.com\/(watch\?v=|embed\/)|youtu.be(\/|%2F))([A-Za-z0-9\-_]+)/g
+			);
+			while ((match = videoRegex.exec(innerHTML)) !== null) {
+				var videoId = match[4];
+				console.log(match, videoId);
 				videoIds.add(videoId);
 			}
 
 			if (videoIds.size == 0) {
 				div.hidden = true;
-				chrome.tabs.sendMessage(tabId, {method: "noVideos"});
+				chrome.tabs.sendMessage(tabId, { method: 'noVideos' });
 			} else {
 				div.hidden = false;
-				link.value = "https://www.youtube.com/watch_videos?video_ids=" + Array.from(videoIds).join(",");
+				link.value =
+					'https://www.youtube.com/watch_videos?video_ids=' +
+					Array.from(videoIds).join(',');
 			}
 		}
 	}
@@ -58,8 +69,8 @@
 
 	function log(message) {
 		chrome.runtime.sendMessage({
-			"method": "log",
-			"message": message
+			method: 'log',
+			message: message,
 		});
 	}
 
