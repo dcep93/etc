2c044b37ffa70134c0a1dd3401ef17379089ed25
Daniel Cepeda <dcep93@gmail.com>
2018-03-30 22:59:12 -0700

    fixed refresh bug

--- syp/extension/background.js
+++ syp/extension/background.js
@@ -1,21 +1,15 @@
 (function() {
 console.log('background');
 
-var tabs = new Set();
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	switch (request.method) {
 		case "init":
-			if (!tabs.has(request.tabId)) {
-				tabs.add(request.tabId);
-				chrome.tabs.executeScript(request.tabId, {file: 'script.js'}, function() {
-					sendResponse({
-						success: true
-					});
+			chrome.tabs.executeScript(request.tabId, {file: 'script.js'}, function() {
+				sendResponse({
+					success: true
 				});
-				return true;
-			}
-			break;
+			});
+			return true;
 		default:
 			console.log(request);
 	}
--- syp/extension/script.js
+++ syp/extension/script.js
@@ -1,17 +1,20 @@
-console.log('script');
+if (!chrome.syp) {
+    console.log('script');
 
-chrome.extension.onMessage.addListener(
-    function(request, sender, sendResponse) {
-    	switch (request.method) {
-    		case "getContent":
-    			sendResponse({success: true, innerHTML: document.body.innerHTML});
-    			break;
-    		case "alert":
-    			alert(request.alert);
-    			break;
-    		default:
-    			console.log(request);
-    	}
-    }
-);
+    chrome.extension.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            switch (request.method) {
+                case "getContent":
+                    sendResponse({success: true, innerHTML: document.body.innerHTML});
+                    break;
+                case "alert":
+                    alert(request.alert);
+                    break;
+                default:
+                    console.log(request);
+            }
+        }
+    );
+    chrome.syp = true;
+}
 
