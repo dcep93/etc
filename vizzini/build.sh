7f7027e3c82c2b344ede544120e6ddab4191c6dd
dcep93 <dan@quizlet.com>
2021-03-22 21:41:02 -0700

    offset

--- vizzini/build.sh
+++ vizzini/build.sh
@@ -10,9 +10,11 @@ video=$folder/video.mp4
 shifted_click=$folder/shifted_click.mp3
 vizzini=$folder/vizzini.mp4
 
+offset=$(ffprobe -v 0 -show_entries stream=start_time -of default=nw=1 /Users/dancepeda/Downloads/vizzini/shifted_click.mp3 | awk -F '=' '{print $2}')
+
 date
 
 python3 finish.py $movie_crop $guitar $sheet $video &&
-  rm $vizzini &&
-  ffmpeg -i $video -i $shifted_click -map 0:0 -map 1:0 -c copy -c:a copy -vcodec libx264 $vizzini &&
-  open $vizzini
+  rm "$vizzini" &&
+  ffmpeg -i "$video" -itsoffset -"$offset" -i "$shifted_click" -map 0:0 -map 1:0 -c copy -c:a copy -vcodec libx264 "$vizzini" &&
+  open "$vizzini"
