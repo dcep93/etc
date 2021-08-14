45b2fa12989af4f9680107bac4d1b3e6c59067f7
dcep93 <dan@quizlet.com>
2021-08-14 09:48:20 -0700

    simpler

--- string_match.py
+++ string_match.py
@@ -7,16 +7,23 @@ warnings.filterwarnings('ignore')
 def find(needle, haystack):
     n = [ord(i) - ord('a') for i in needle]
     h = [ord(i) - ord('a') for i in haystack]
-    multiplied = get_multiplied_fft(n[::-1], h)
     n2 = sum([i**2 for i in n])
-    h2 = sum([i**2 for i in h[:len(n) - 1]])
+    h2i = get_h2i(h, len(n))
+    multiplied = get_multiplied_fft(n[::-1], h)
     for i in range(len(h) - len(n) + 1):
-        j = i + len(n) - 1
-        h2 += h[j]**2
-        if 2 * multiplied[j] == n2 + h2:
+        if 2 * multiplied[i] == n2 + h2i[i]:
             return i
-        h2 -= h[j - len(n) + 1]**2
     return -1
+
+
+def get_h2i(h, l):
+    h2 = sum([i**2 for i in h[:l - 1]])
+    h2i = []
+    for i in range(len(h) - l + 1):
+        h2 += h[i + l - 1]**2
+        h2i.append(h2)
+        h2 -= h[i]**2
+    return h2i
 
 
 def get_multiplied_fft(a, b):
@@ -24,4 +31,4 @@ def get_multiplied_fft(a, b):
     a_f = rfft(a, L)
     b_f = rfft(b, L)
     raw = irfft(a_f * b_f)
-    return [int(round(i)) for i in raw][:-1]
+    return [int(round(i)) for i in raw][:-1][len(a) - 1:]
