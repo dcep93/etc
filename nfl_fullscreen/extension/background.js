755122e6a97c42638b63f50000d9edd0e656aa39
dcep93 <dan@quizlet.com>
2021-09-13 17:16:26 -0700

    x

--- nfl_fullscreen/extension/background.js
+++ nfl_fullscreen/extension/background.js
@@ -1,6 +1,7 @@
 console.log("background", new Date());
 
 chrome.browserAction.onClicked.addListener((tab) =>
-  chrome.tabs.sendMessage(tab.id, {tabId: tab.id}, muted => 
-    chrome.tabs.update(tab.id, { muted }))
+  chrome.tabs.sendMessage(tab.id, { tabId: tab.id }, (muted) =>
+    chrome.tabs.update(tab.id, { muted })
+  )
 );
--- nfl_fullscreen/extension/content_script.js
+++ nfl_fullscreen/extension/content_script.js
@@ -4,25 +4,22 @@ var muted = undefined;
 
 function execute(message, sender, sendResponse) {
   if (muted === undefined) {
-    fullscreen(document.getElementById('video-player'));
-    fullscreen(document.getElementsByTagName('iframe')[0]);
+    Array.from(document.getElementsByTagName("iframe")).find(
+      (i) => i.src && i.src !== "about:blank"
+    ).style = `
+    position: fixed;
+    right: 0px;
+    bottom: 0px;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: 100;
+    `;
+
     muted = false;
   } else {
     muted = !muted;
-    sendResponse(muted);
   }
-}
-
-function fullscreen(element) {
-  console.log(element);
-  element.style = `
-position: fixed;
-right: 0px;
-bottom: 0px;
-min-width: 100%;
-min-height: 100%;
-width: auto;
-height: auto;
-z-index: 100;
-`;
+  sendResponse(muted);
 }
