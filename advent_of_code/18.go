362e27b20c0686c51f9db2306726ba2c6c0485be
dcep93 <dcep93@gmail.com>
2022-12-18 17:24:24 -0500

    x

---
+++ advent_of_code/18.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func _18() {
+	lines := getLines("18.txt")
+
+	cubes := make(map[string][]int)
+
+	for _, line := range lines {
+		var cube []int
+		parts := strings.Split(line, ",")
+		for _, part := range parts {
+			point, err := strconv.Atoi(part)
+			if err != nil {
+				panic(err)
+			}
+			cube = append(cube, point)
+		}
+		cubes[fmt.Sprintf("%v", cube)] = cube
+	}
+
+	part1 := func() {
+		return
+		s := 0
+		for _, cube := range cubes {
+			for i := -1; i <= 1; i += 2 {
+				for j := range cube {
+					c := append([]int{}, cube...)
+					c[j] += i
+					if _, ok := cubes[fmt.Sprintf("%v", c)]; !ok {
+						s += 1
+					}
+				}
+			}
+		}
+		fmt.Println(s)
+	}
+
+	part2 := func() {
+		outsides := make(map[string]int)
+		var mins []int
+		var maxes []int
+		for _, cube := range cubes {
+			maxes = append(maxes, cube...)
+			mins = append(mins, cube...)
+			break
+		}
+		for _, cube := range cubes {
+			for i, v := range cube {
+				if maxes[i] < v {
+					maxes[i] = v
+				}
+				if mins[i] > v {
+					mins[i] = v
+				}
+			}
+		}
+		var isOutside func(cube []int) int
+		isOutside = func(cube []int) int {
+			for i, v := range cube {
+				if v > maxes[i] || v < mins[i] {
+					return 1
+				}
+			}
+			key := fmt.Sprintf("%v", cube)
+			outside, ok := outsides[key]
+			if ok {
+				return outside
+			}
+			outsides[key] = 2
+			unsure := false
+			for i := -1; i <= 1; i += 2 {
+				for j := range cube {
+					c := append([]int{}, cube...)
+					c[j] += i
+					if _, ok := cubes[fmt.Sprintf("%v", c)]; !ok {
+						outside := isOutside(c)
+						if outside == 1 {
+							outsides[key] = 1
+							return 1
+						}
+						if outside == 2 {
+							unsure = true
+						}
+					}
+				}
+			}
+			if unsure {
+				delete(outsides, key)
+			} else {
+				outsides[key] = 0
+			}
+			return 0
+		}
+		s := 0
+		for _, cube := range cubes {
+			fmt.Println(cube)
+			for i := -1; i <= 1; i += 2 {
+				for j := range cube {
+					c := append([]int{}, cube...)
+					c[j] += i
+					if _, ok := cubes[fmt.Sprintf("%v", c)]; !ok {
+						if isOutside(c) == 1 {
+							s += 1
+						}
+					}
+				}
+			}
+		}
+		fmt.Println(s)
+	}
+
+	part1()
+	part2() // 2551 low
+
+	fmt.Println("done")
+}
