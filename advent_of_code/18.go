6bde483ae25ac5ccefb04b3372cb291dced5af77
dcep93 <dcep93@gmail.com>
2022-12-18 19:13:39 -0500

    x

--- advent_of_code/18.go
+++ advent_of_code/18.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strconv"
 	"strings"
 )
@@ -52,87 +51,54 @@ func _18() {
 		}
 		for _, cube := range cubes {
 			for i, v := range cube {
-				if maxes[i] < v {
-					maxes[i] = v
+				if maxes[i] <= v {
+					maxes[i] = v + 1
 				}
-				if mins[i] > v {
-					mins[i] = v
+				if mins[i] >= v {
+					mins[i] = v - 1
 				}
 			}
 		}
-		outsides := make(map[string]int)
-		var isOutside func(cube []int, depth int) int
-		isOutside = func(cube []int, depth int) int {
-			if depth > 100 {
-				fmt.Println("failed", cube, depth)
-				os.Exit(0)
-			}
-			for i, v := range cube {
-				if v > maxes[i] || v < mins[i] {
-					return 1
+		outsides := make(map[string]bool)
+		search := [][]int{mins}
+		for gen := 0; len(search) > 0; gen++ {
+			// fmt.Println(len(search))
+			var nextSearch [][]int
+			for _, cube := range search {
+				key := fmt.Sprintf("%v", cube)
+				if outsides[key] {
+					continue
 				}
-			}
-			key := fmt.Sprintf("%v", cube)
-			outside, ok := outsides[key]
-			if ok {
-				return outside
-			}
-			outsides[key] = 2
-			unsure := false
-			for i := -1; i <= 1; i += 2 {
-				for j := range cube {
-					c := append([]int{}, cube...)
-					c[j] += i
-					if _, ok := cubes[fmt.Sprintf("%v", c)]; !ok {
-						outside := isOutside(c, depth+1)
-						if outside == 1 {
-							outsides[key] = 1
-							return 1
+				outsides[key] = true
+				for i := -1; i <= 1; i += 2 {
+					for j := range cube {
+						c := append([]int{}, cube...)
+						c[j] += i
+						if c[j] < mins[j] || c[j] > maxes[j] {
+							continue
 						}
-						if outside == 0 {
-							outsides[key] = 0
-							return 0
-						}
-						if outside == 2 {
-							unsure = true
+						if _, ok := cubes[fmt.Sprintf("%v", c)]; !ok {
+							nextSearch = append(nextSearch, c)
 						}
 					}
 				}
 			}
-			if unsure {
-				delete(outsides, key)
-				return 2
-			}
-			outsides[key] = 0
-			return 0
+			search = nextSearch
 		}
 		s := 0
-		// for _, cube := range cubes {
-		for _, line := range lines {
-			var cube []int
-			parts := strings.Split(line, ",")
-			for _, part := range parts {
-				point, err := strconv.Atoi(part)
-				if err != nil {
-					panic(err)
-				}
-				cube = append(cube, point)
-			}
-
+		for _, cube := range cubes {
 			// fmt.Println(cube)
 			for i := -1; i <= 1; i += 2 {
 				for j := range cube {
 					c := append([]int{}, cube...)
 					c[j] += i
-					if _, ok := cubes[fmt.Sprintf("%v", c)]; !ok {
-						if isOutside(c, 0) == 1 {
-							s += 1
-						}
+					if _, ok := outsides[fmt.Sprintf("%v", c)]; ok {
+						s += 1
 					}
 				}
 			}
 		}
-		// fmt.Println(s)
+		fmt.Println(s)
 	}
 
 	part1()
