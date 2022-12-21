66b0aea63a64ca5bf75b125988442418365d5115
dcep93 <dcep93@gmail.com>
2022-12-21 13:02:28 -0800

    x

--- advent_of_code/20.go
+++ advent_of_code/20.go
@@ -92,15 +92,27 @@ func _20() {
 
 	part2 := func() {
 		key := 811589153
+		width := 3
 		head := &node{}
-		var q []*node
 		prev := head
-		for _, line := range lines {
+		var q []*node
+		r_anchors := map[*node]int{head: 0}
+		anchors := map[int]*node{0: head}
+		var as []int
+		for i, line := range lines {
 			value, err := strconv.Atoi(line)
 			if err != nil {
 				panic(err)
 			}
-			n := &node{prev: prev, value: value * key}
+			n := &node{
+				prev:  prev,
+				value: value * key,
+			}
+			if i != 0 && i%width == 0 {
+				anchors[i] = n
+				r_anchors[n] = i
+				as = append(as, i)
+			}
 			if prev != nil {
 				prev.next = n
 			}
@@ -109,19 +121,6 @@ func _20() {
 		}
 		prev.next = head
 		head.prev = prev
-		var gen func(n *node, isPositive bool) *node
-		gen = func(n *node, isPositive bool) *node {
-			var nn *node
-			if isPositive {
-				nn = n.next
-			} else {
-				nn = n.prev
-			}
-			if nn == head {
-				return gen(nn, isPositive)
-			}
-			return nn
-		}
 		p := func() {
 			var s []int
 			for n := head.next; n != head; n = n.next {
@@ -130,31 +129,62 @@ func _20() {
 			fmt.Println(s)
 		}
 		m := len(q) - 1
-		for i := 0; i < 1; i++ {
+		getNN := func(n *node) *node {
+			v := n.value
+			for v < 0 {
+				v += m
+			}
+			anchor := n
+			for {
+				if v == 0 {
+					return anchor
+				}
+				if _, ok := r_anchors[anchor]; ok {
+					break
+				}
+				anchor = anchor.next
+				v--
+			}
+			r := r_anchors[anchor]
+			target := (r + v) % m
+			closest := -1
+			for _, a := range as {
+				if a <= target {
+					closest = a
+					if r < a {
+						aa := anchors[a]
+						delete(r_anchors, aa)
+						aa = aa.next
+						anchors[a] = aa
+						r_anchors[aa] = a
+
+					}
+				} else if r > a {
+					aa := anchors[a]
+					delete(r_anchors, aa)
+					aa = aa.prev
+					anchors[a] = aa
+					r_anchors[aa] = a
+				}
+			}
+			nn := anchors[closest]
+			diff := target - closest
+			for i := 0; i < diff; i++ {
+				nn = nn.next
+			}
+			return nn
+		}
+		for i := 0; i < 10; i++ {
 			fmt.Println(i)
 			p()
+			fmt.Println()
 			for _, n := range q {
-				v := n.value
-				for v < 0 {
-					v += m
-				}
-				for v > m {
-					v -= m
-				}
-				if v == 0 {
+				nn := getNN(n)
+				if nn == n {
 					continue
 				}
-				isPositive := v < (m / 2)
-				if !isPositive {
-					v = m - v
-				}
-				fmt.Println(v, isPositive, n)
-				nn := n
-				nn.prev.next = nn.next
-				nn.next.prev = nn.prev
-				for i := 0; i < v; i++ {
-					nn = gen(nn, isPositive)
-				}
+				n.prev.next = n.next
+				n.next.prev = n.prev
 				n.prev = nn
 				n.next = nn.next
 				nn.next.prev = n
@@ -162,18 +192,17 @@ func _20() {
 			}
 		}
 		p()
-		return
 		n := head
-		for {
-			n = gen(n, true)
-			if n.value == 0 {
-				break
-			}
+		for n.value != 0 {
+			n = n.next
 		}
 		s := 0
 		for i := 0; i < 3; i++ {
 			for j := 0; j < 1000; j++ {
-				n = gen(n, true)
+				n = n.next
+				if n == head {
+					n = n.next
+				}
 			}
 			fmt.Println(n)
 			s += n.value
