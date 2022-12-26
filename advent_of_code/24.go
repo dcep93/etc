e6a7db3967a800c843b93c056f91b63b612bb87e
dcep93 <dcep93@gmail.com>
2022-12-25 23:16:34 -0800

    x

--- advent_of_code/24.go
+++ advent_of_code/24.go
@@ -7,6 +7,111 @@ import (
 func _24() {
 	lines := getLines("24.txt")
 
+	width := len(lines[0])
+	height := len(lines)
+
+	var start int
+	for i := 0; i <= width; i++ {
+		if lines[0][i] == '.' {
+			start = i
+			break
+		}
+	}
+	var target int
+	for i := 0; i <= width; i++ {
+		if lines[height-1][i] == '.' {
+			target = i
+			break
+		}
+	}
+	search := map[string][]int{"": {start, 0}}
+
+	bs := [][]int{}
+	dMap := map[rune]int{'>': 0, 'v': 1, '<': 2, '^': 3}
+	for j, line := range lines {
+		for i, cell := range line {
+			if cell == '#' || cell == '.' {
+				continue
+			}
+			direction := dMap[cell]
+			bs = append(bs, []int{i, j, direction})
+		}
+	}
+
+	directions := [][]int{
+		{1, 0},
+		{0, 1},
+		{-1, 0},
+		{0, -1},
+		{0, 0},
+	}
+
+	bigSeen := make(map[string]bool)
+	for steps := 1; steps < 10000; steps++ {
+		fmt.Println(steps, len(search))
+		if len(search) == 0 {
+			panic("impossible")
+		}
+		seen := make(map[string]bool)
+		for _, b := range bs {
+			d := directions[b[2]]
+			for i, x := range d {
+				b[i] += x
+			}
+			if b[0] == 0 {
+				b[0] = width - 2
+			}
+			if b[0] == width-1 {
+				b[0] = 1
+			}
+			if b[1] == 0 {
+				b[1] = height - 2
+			}
+			if b[1] == height-1 {
+				b[1] = 1
+			}
+			bb := []int{b[0], b[1]}
+			seen[v(bb)] = true
+		}
+		bbs := v(bs)
+		nextSearch := make(map[string][]int)
+		for _, s := range search {
+			vv := v(s) + bbs
+			if bigSeen[vv] {
+				continue
+			}
+			bigSeen[vv] = true
+			for _, d := range directions {
+				ss := append([]int{}, s...)
+				for i, x := range d {
+					ss[i] += x
+				}
+				if ss[0] == 0 || ss[0] == width-1 {
+					continue
+				}
+				if ss[1] <= 0 {
+					if ss[0] != start || ss[1] != 0 {
+						continue
+					}
+				}
+				if ss[1] == height-1 {
+					if ss[0] == target {
+						fmt.Println(steps)
+						return
+					}
+					continue
+				}
+				key := v(ss)
+				if seen[key] {
+					continue
+				}
+				nextSearch[key] = ss
+			}
+		}
+		search = nextSearch
+	}
+	panic("failed")
+
 	part1 := func() {
 	}
 
