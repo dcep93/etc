a1880ed75965a08e4230f82e96d2eb9352938fa1
dcep93 <dcep93@gmail.com>
2023-06-16 14:50:54 -0400

    x

--- generic_extension/background.js
+++ generic_extension/background.js
@@ -7,32 +7,44 @@ chrome.runtime.onMessageExternal.addListener(function (
   _,
   sendResponse
 ) {
-  if (request.storage) {
-    if (request.storage.action === "get") {
-      chrome.storage.local.get(request.storage.keys, (result) => {
-        sendResponse(result);
+  return execute(request)
+    .then((result) => sendResponse(result))
+    .catch((err) => sendResponse({ ok: false, err: err.message }));
+});
+
+function execute(request) {
+  return new Promise((resolve, reject) => {
+    if (request.storage) {
+      if (request.storage.action === "get") {
+        console.log("get", request.storage.keys);
+        chrome.storage.local.get(request.storage.keys, (result) =>
+          resolve({ ok: true, result })
+        );
+      }
+      if (request.storage.action === "save")
+        chrome.storage.local.set(request.storage.save, () =>
+          resolve({ ok: true })
+        );
+    }
+    if (request.fetch) {
+      const key = request.fetch.url;
+      const cached = fetch_cache[key];
+      const now = Date.now();
+      if (now - cached?.timestamp < request.fetch.maxAgeMs)
+        return resolve(cached.cache);
+      return fetch(request.fetch.url, request.fetch.options).then((resp) => {
+        if (!resp.ok)
+          return resp.text().then((err) => resolve({ err, ...resp }));
+        (request.fetch.json ? resp.json() : resp.text())
+          .then((msg) => ({ msg, ok: true }))
+          .then((cache) => {
+            if (!request.fetch.noCache) {
+              fetch_cache[key] = { timestamp: now, cache };
+            }
+            return cache;
+          })
+          .then(resolve);
       });
     }
-    if (request.storage.action === "save") {
-      chrome.storage.local.set(request.storage.save, () => sendResponse(true));
-    }
-  }
-  if (request.fetch) {
-    const key = JSON.stringify(request.fetch);
-    const cached = fetch_cache[key];
-    const now = Date.now();
-    if (now - cached?.timestamp < request.fetch.maxAgeMs)
-      return sendResponse(cached.cache);
-    return fetch(request.fetch.url, request.fetch.options).then((resp) => {
-      if (!resp.ok)
-        return resp.text().then((text) => sendResponse({ text, ...resp }));
-      (request.fetch.json ? resp.json() : resp.text())
-        .then((msg) => ({ msg, ok: true }))
-        .then((cache) => {
-          fetch_cache[key] = { timestamp: now, cache };
-          return cache;
-        })
-        .then(sendResponse);
-    });
-  }
-});
+  });
+}
