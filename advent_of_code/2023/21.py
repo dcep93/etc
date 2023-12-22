6f9639f2745bb0d9f7b3163f401df3925f00304e
dcep93 <dcep93@gmail.com>
2023-12-22 12:23:20 -0800

    x

--- advent_of_code/2023/21.py
+++ advent_of_code/2023/21.py
@@ -6,23 +6,23 @@ with open(f'{__file__.split("/")[-1].split(".py")[0]}.txt') as fh:
     txt = fh.read()
 lines = [list(line) for line in txt.split("\n")]
 
-# for i in range(63):
+# for i in range(50):
 #     lines[i] = list(lines[0])
 #     lines[-i-1] = list(lines[0])
-# for _ in range(61):
+# for _ in range(0):
 #     lines = [l[1:-1] for l in lines[1:-1]]
 
-lines = '''
-.........
-.........
-#........
-.##......
-....S....
-.........
-.........
-.........
-.........
-'''.strip().split("\n")
+# lines = '''
+# #.....#..
+# #....#...
+# #........
+# ####.####
+# ....S....
+# .........
+# .........
+# .........
+# .........
+# '''.strip().split("\n")
 
 
 # l = ['.' for _ in range(5)]
@@ -52,96 +52,91 @@ def getAnswer(isPart1: bool):
         if p < 0 or p >= len(v):
             return None
         return get(s, v[p], isPart1)
-        
-    steps = 8*len(lines)+3 # 26501365
-    start = getPosition()
-    spots = [start]
-    seen = {start: True}
-    starts = {tuple(start[i] + 2*nn[i] for i in range(len(start))): False for nn in [
-        (len(lines), 0),
-        (-len(lines), 0),
-        (0, len(lines[0])),
-        (0, -len(lines[0])),
-    ]}
 
-    scale = None
-    oldseen = {}
-    oldspots = []
-    corners = None
-
-    memo = {}
-
-    def f(ii, isFinal=False):
-        parity = (sum(start) + ii) % 2
-        counts = [sum([1 for s in ss if (sum(s) % 2) == parity]) for ss in [seen.keys(), oldseen.keys(), spots, oldspots]]
-        if isFinal:
-            print('counts', counts)
-            # print(seen)
-        if scale is None:
-            return counts[0]
-        if counts[3] > 0:
-            raise
-        s_old = counts[1] * scale * scale
-        s_new = (counts[0] - counts[1]) * scale - (corners * (scale - 1))
-        total = s_old + s_new
-        print('t', scale, corners, 'total', total, s_old, s_new)
-        # 
-        return total
-        # 606822764898251 too low
-        # 615479116147000 too high
-
-    ii = 0
-    while ii < steps:
-        if scale is None:
-            memo[ii] = f(ii)
-        if all(starts.values()):
-            import math
-            lcm = 1
-            for iii in starts.values():
-                lcm = (lcm * iii) // math.gcd(lcm, iii)
-            # print(ii, lcm, starts)
-            if any(v != lcm for v in starts.values()):
-                raise
-            if ii != lcm:
-                raise
-            for i, line in enumerate(lines):
-                for j, char in enumerate(line):
-                    if (char == '#') == (i, j) in seen:
-                        raise
-            starts = {start: False}
-            oldseen = {i: True for i in seen.keys()}
-            oldspots = spots
-            if isPart1:
-                continue # 318804 318800
-            if steps <= lcm * 2:
-                continue
-            scale = ((steps-1) // lcm)
-            ii = scale * lcm
-            corners = memo[steps - ii]
-            print('remaining', steps - ii, corners, {i: memo[i] for i in range(steps-ii-1, steps-ii+2)})
-            print('lcm', lcm, steps - ii, steps)
-            continue
-        ii += 1
-        if not spots:
-            raise
+    def getSpots():
         next_spots = []
         for s in spots:
             for d in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 n = tuple(s[i] + d[i] for i in range(len(s)))
                 if n in seen:
                     continue
-                v = get(n, lines, False)
+                v = get(n, lines, False) # isPart1)
                 if v == 'S':
                     v = '.'
-                if n in starts:
-                    starts[n] = ii
+                if n in targets:
+                    targets[n] = ii
                 if v != '.':
                     continue
                 seen[n] = True
                 next_spots.append(n)
-        spots = next_spots
+        return next_spots
 
-    return f(steps, True)
+    def scaleSpots():
+        import math
+        lcm = 1
+        for iii in targets.values():
+            lcm = (lcm * iii) // math.gcd(lcm, iii)
+        # print(ii, lcm, targets)
+        if any(v != lcm for v in targets.values()):
+            raise
+        # if ii != lcm:
+        #     raise
+        for i, line in enumerate(lines):
+            for j, char in enumerate(line):
+                if (char == '#') == (i, j) in seen:
+                    raise
+
+        factor = lcm // 2
+        radius = factor // 2
+        scale = steps // factor
+        remainder = (steps - (scale * factor))
+
+        extracted = [(memo[i]-memo[i-1], memo[i+radius]-memo[i+radius-1], scale + (0 if remainder >= i else -1), (remainder-i) % radius) for i in range(radius)]
+        x = [radius * count * (base + (diff * (count - 1) // 2)) + (extra * (base + (diff * (count*+1)))) for base, diff, count, extra in extracted]
+        return sum(x)
+
+        # 606822764898251 too low
+        # 615479116147000 too high
+
+    steps = 10*len(lines)+11
+    start = getPosition()
+    targets = {tuple(start[i] + 4*nn[i] for i in range(len(start))): False for nn in [
+        (len(lines), 0),
+        (-len(lines), 0),
+        (0, len(lines[0])),
+        (0, -len(lines[0])),
+    ]}
+    spots = [start]
+    seen = {start: True}
+    
+    ii = 0
+    parity = (sum(start) + steps) % 2
+    if parity == 1:
+        ii += 1
+        spots = getSpots()
+
+    total = 0
+    memo = {-1:0}
+    while True:
+        num = len(spots)
+        memo[(ii-1)//2] = num
+        total += num
+
+        if ii >= steps:
+            break
+
+        if isPart1 and all(targets.values()):
+            return scaleSpots()
+
+        if not spots:
+            print(ii)
+            raise
+
+        for _ in range(2):
+            ii += 1
+            spots = getSpots()
+
+    return total
 
 print(getAnswer(True))
 print(getAnswer(False))
